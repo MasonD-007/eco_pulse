7054caf1dea596d2fd7347c54dcb1f195403a291
Kyle <kylefossen21@gmail.com>
2025-01-16 19:43:54 -0800

    added onboarding page

---
+++ Eco Pulse/Eco Pulse/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Eco Pulse
+//
+//  Created by Kyle Fossen on 1/16/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
