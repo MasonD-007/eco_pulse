82d91c68f38b422bff6de482e83c86d833c340e1
MasonD-007 <masondrake305@gmail.com>
2025-03-11 16:37:10 -0700

    Made landing page

---
+++ .next/static/chunks/app_page_tsx_04199f._.js
@@ -0,0 +1,12 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/app_page_tsx_04199f._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/app_page_tsx_04199f._.js",
+  "chunks": [
+    "static/chunks/_3d7285._.js",
+    "static/chunks/node_modules_b59b67._.js"
+  ],
+  "source": "dynamic"
+});
